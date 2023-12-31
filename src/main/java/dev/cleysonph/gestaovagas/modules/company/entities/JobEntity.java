36a65400cc9878c49f7055d86cf4f503c212ef9b
Cleyson <cleysonph@gmail.com>
2023-12-31 15:19:38 -0300

    entidade job

--- src/main/java/dev/cleysonph/gestaovagas/modules/company/entities/JobEntity.java
+++ src/main/java/dev/cleysonph/gestaovagas/modules/company/entities/JobEntity.java
@@ -5,39 +5,35 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
 @Entity
-public class CompanyEntity {
+public class JobEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @NotBlank
-    @Pattern(regexp = "\\S+", message = "cannot contain blank spaces")
-    private String username;
-
-    @Email
-    private String email;
-
-    @Size(min = 10, max = 100)
-    private String password;
-
-    private String website;
-
-    private String name;
-
     private String description;
+
+    private String benefits;
+
+    private String level;
+
+    @Column(name = "company_id")
+    private UUID companyId;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private CompanyEntity company;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
