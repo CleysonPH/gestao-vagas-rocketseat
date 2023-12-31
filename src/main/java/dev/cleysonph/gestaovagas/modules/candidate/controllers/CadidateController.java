9dd06b404356ad28c9be240913c12d8f0afc5479
Cleyson <cleysonph@gmail.com>
2023-12-31 14:35:28 -0300

    criando repositório

--- src/main/java/dev/cleysonph/gestaovagas/modules/candidate/controllers/CadidateController.java
+++ src/main/java/dev/cleysonph/gestaovagas/modules/candidate/controllers/CadidateController.java
@@ -1,20 +1,25 @@
 package dev.cleysonph.gestaovagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.cleysonph.gestaovagas.modules.candidate.CandidateEntity;
+import dev.cleysonph.gestaovagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidates")
 public class CadidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping
-    public void create(@RequestBody @Valid CandidateEntity candidateEntity) {
-        System.out.println(candidateEntity);
+    public CandidateEntity create(@RequestBody @Valid CandidateEntity candidateEntity) {
+        return candidateRepository.save(candidateEntity);
     }
     
 }
