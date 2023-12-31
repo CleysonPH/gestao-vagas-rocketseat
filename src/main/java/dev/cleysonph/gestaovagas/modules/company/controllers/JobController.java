6c5f63300827b2af312a5b89a939bb269725d7b9
Cleyson <cleysonph@gmail.com>
2023-12-31 16:04:44 -0300

    job controller

---
+++ src/main/java/dev/cleysonph/gestaovagas/modules/company/controllers/JobController.java
@@ -0,0 +1,25 @@
+package dev.cleysonph.gestaovagas.modules.company.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.cleysonph.gestaovagas.modules.company.entities.JobEntity;
+import dev.cleysonph.gestaovagas.modules.company.usecases.CreateJobUseCase;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+
+    @Autowired
+    private CreateJobUseCase createJobUseCase;
+
+    @PostMapping
+    public void create(@RequestBody @Valid JobEntity jobEntity) {
+        createJobUseCase.execute(jobEntity);
+    }
+    
+}
