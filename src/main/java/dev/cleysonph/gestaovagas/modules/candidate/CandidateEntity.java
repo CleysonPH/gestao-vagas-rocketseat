51d6288524fe1dc821638ee3875927f916fea821
Cleyson <cleysonph@gmail.com>
2023-12-31 14:31:41 -0300

    criando tabela de candidato

--- src/main/java/dev/cleysonph/gestaovagas/modules/candidate/CandidateEntity.java
+++ src/main/java/dev/cleysonph/gestaovagas/modules/candidate/CandidateEntity.java
@@ -1,15 +1,25 @@
 package dev.cleysonph.gestaovagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Entity
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String name;
@@ -26,5 +36,8 @@ public class CandidateEntity {
     private String description;
 
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
     
 }
