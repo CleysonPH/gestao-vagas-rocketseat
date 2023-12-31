1329b00d6606780aad2f14d4793d304b946df4ef
Cleyson <cleysonph@gmail.com>
2023-12-31 13:53:10 -0300

    criando estrutura do projeto

---
+++ src/main/java/dev/cleysonph/gestaovagas/modules/candidate/controllers/CadidateController.java
@@ -0,0 +1,19 @@
+package dev.cleysonph.gestaovagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.cleysonph.gestaovagas.modules.candidate.CandidateEntity;
+
+@RestController
+@RequestMapping("/candidates")
+public class CadidateController {
+
+    @PostMapping
+    public void create(@RequestBody CandidateEntity candidateEntity) {
+        System.out.println(candidateEntity);
+    }
+    
+}
