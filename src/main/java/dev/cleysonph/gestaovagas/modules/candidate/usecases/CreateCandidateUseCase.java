e60f19de48777c577e71f07e8b81cae6501f4d17
Cleyson <cleysonph@gmail.com>
2024-01-01 01:40:01 -0300

    hash da senha do candidato

--- src/main/java/dev/cleysonph/gestaovagas/modules/candidate/usecases/CreateCandidateUseCase.java
+++ src/main/java/dev/cleysonph/gestaovagas/modules/candidate/usecases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package dev.cleysonph.gestaovagas.modules.candidate.usecases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import dev.cleysonph.gestaovagas.exceptions.UserFoundException;
@@ -13,11 +14,16 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
             .ifPresent(candidate -> {
                 throw new UserFoundException();
             });
+        var encodedPassword = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(encodedPassword);
         return candidateRepository.save(candidateEntity);
     }
     
